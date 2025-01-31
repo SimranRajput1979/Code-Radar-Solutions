e8ad239aeba36091fa105b87e6b921d7bada81f6
Simran <rajputsimran.0079@gmail.com>
2025-02-01 01:36:54 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,29 +1,57 @@
+// #include <stdio.h>
+// #include <string.h>
+// int main(){
+//     int a,b;
+//     char c[2];
+//     scanf("%d %d %c",&a,&b,&c);
+//     if (strcmp(c,"+")==0){
+//         printf("%d\n",a+b);
+//     }
+//     else if (strcmp(c,"-")==0){
+//         printf("%d\n",a-b);
+//     }
+//     else if (strcmp(c,"*")==0){
+//         printf("%d\n",a*b);
+//     }
+//     else if (strcmp(c,"/")==0){
+//         if (b!=0){
+//             printf("%d\n",a/b);
+//         }
+//         else{
+//             printf("error");
+//         }
+//     }
+//     else{
+//         printf("error");
+//     }
+//     return 0;
+
+// } this code is also corrected
+
 #include <stdio.h>
-#include <string.h>
 int main(){
     int a,b;
-    char c[2];
+    char c;
     scanf("%d %d %c",&a,&b,&c);
-    if (strcmp(c,"+")==0){
-        printf("%d\n",a+b);
+    if (c=="+"){
+        printf("%d",a+b);
     }
-    else if (strcmp(c,"-")==0){
-        printf("%d\n",a-b);
+    else if (c=="-"){
+        printf("%d",a-b);
     }
-    else if (strcmp(c,"*")==0){
-        printf("%d\n",a*b);
+    else if (c=="*"){
+        printf("%d",a*b);
     }
-    else if (strcmp(c,"/")==0){
+    else if (c=="/"){
         if (b!=0){
-            printf("%d\n",a/b);
+            printf("%d",a/b);
         }
         else{
             printf("error");
         }
-    }
     else{
         printf("error");
     }
-    return 0;
-
+    return 0;    
+    }
 }
