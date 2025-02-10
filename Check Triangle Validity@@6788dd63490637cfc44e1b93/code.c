10056dfaf3cd98adcc70a0f4477f815a555fb93b
Simran <rajputsimran.0079@gmail.com>
2025-02-10 16:34:21 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -2,7 +2,7 @@
 int main(){
     int a,b,c;
     scanf("%d %d %d",&a,&b,&c);
-    if ((a^2+b^2==c^2)||(b^2+c^2==a^2)||(c^2+a^2==b^2)){
+    if ((a*a+b*b==c*c)||(b*b+c*c==a*a)||(c*c+a*a==b*b)){
         printf("Valid");
     }
     else{
