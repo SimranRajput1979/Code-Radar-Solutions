c547f3160ac7b85d96cc032652c1ceeb708033ee
Simran <rajputsimran.0079@gmail.com>
2025-04-07 20:24:37 +0530

    Solution to Insertion Sort for Strings

--- Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
+++ Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
@@ -3,7 +3,6 @@ insertionSort(char arr[],int n){
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
             if (strcmp(arr[j],arr[j+1])>0){
-                char temp;
                 strcpy(temp,arr[j]);
                 strcpy(arr[j],arr[j+1]);
                 strcpy(arr[j+1],temp);
