786ea0efe73af36087e65cfcb70667d534f9b653
Simran <rajputsimran.0079@gmail.com>
2025-04-11 10:25:22 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,14 +1,5 @@
 // Your code here...
 #include<stdio.h>
-// int palindrome(int num){
-//     int original=num,reversed=0;
-//     while(num>0){
-//         int result=num%10;
-//         reversed=reversed*10+result;
-//         num=num/10;
-//     }
-//     original==reversed;
-// }
 int main(){
     int n;
     scanf("%d",&n);
@@ -20,10 +11,23 @@ int main(){
     scanf("%d",&k);
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
-        if((arr[i]+arr[j])==k){
-            if(i<j){
-            printf("%d %d\n",arr[i],arr[j]);
-        }}
-    }}
-     return 0;
+                if(arr[i]+arr[j]==k){
+                int alreadyprinted=0;
+                for(int m;m<i;m++){
+                    for(int n=m+1;n<j;j++){
+                        if((arr[m]==arr[i] && arr[n]==arr[j]) || arr[m]==arr[j] && arr[n]==arr[i]){
+                            alreadyprinted=1;
+                            break;
+                        }
+                    }
+                    if(alreadyprinted)break;
+                }
+                if(!alreadyprinted){
+                    printf("%d %d\n",arr[i],arr[j]);
+                }
+        }
+    }
+   
+}
+return 0;
 }
